98a4d6786f560dacf26382783877dbb4ad3ceb86
Parmar Jay <jaykumarvinodbhi@gmail.com>
2023-08-29 10:05:37 +0530

    jhygfe

---
+++ src/Components/About.jsx
@@ -0,0 +1,14 @@
+// import MyTable from "./FezanBhaiTask/Fezan"
+import Fezan2 from "./FezanBhaiTask/Fezan2"
+
+const About = () => {
+  return (
+    <div>
+      {/* <MyTable/> */}
+      <Fezan2/>
+
+    </div>
+  )
+}
+
+export default About
